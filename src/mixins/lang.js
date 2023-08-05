af45fa844e5639fdadc12c74d61cb16c490934e9
Wampie Driessen <1059058+wampiedriessen@users.noreply.github.com>
2023-08-05 18:47:57 +0200

    Update src/mixins/lang.js

--- src/mixins/lang.js
+++ src/mixins/lang.js
@@ -30,7 +30,7 @@ export default {
             localStorage.locale = lang;
             setPageLocale();
         },
-        /** 
+        /**
          * Change the language for the current page (no localstore set)
          * @param {string} lang Code of language to switch to.
          */
