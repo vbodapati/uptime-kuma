b71e7e1c6d8bb5b8f52afa9c6d92058f4f7987fe
Filipe Veloso <filipe.roman.veloso@gmail.com>
2023-11-04 22:31:21 -0300

    add edit user test

--- test/cypress/e2e/settings.cy.js
+++ test/cypress/e2e/settings.cy.js
@@ -1,6 +1,7 @@
 const actor = require("../support/actors/actor");
 const userData = require("../support/const/user-data");
 const settingsUsersAddPage = require("../support/pages/settings-users-add-page");
+const settingsUsersEditPage = require("../support/pages/settings-users-edit-page");
 
 describe("user can create a new account on settings page", () => {
     before(() => {
@@ -16,13 +17,34 @@ describe("user can create a new account on settings page", () => {
     });
 });
 
-describe("logout and login with new user", () => {
+describe("login with new user then edit info", () => {
     before(() => {
         cy.visit('/');
         actor.actor.loginTask.fillAndSubmitLoginForm(userData.ADMIN_USER_DATA.username, userData.ADMIN_USER_DATA.password);
     });
     it("user can edit its data with new created account", () => {
         cy.visit(`/settings/users/edit/${userData.ADMIN_USER_DATA.id}`);
+    
+        actor.actor.editUserTask.fillAndSubmitNewUserNameForm(userData.ADMIN_USER_DATA.username + "new");
+        actor.actor.editUserTask.fillAndSubmitNewPasswordForm(userData.ADMIN_USER_DATA.password, userData.ADMIN_USER_DATA.password + "new", userData.ADMIN_USER_DATA.password + "new");
+        
+        cy.wait(1000);
+        cy.visit('/');
+        actor.actor.loginTask.fillAndSubmitLoginForm(userData.ADMIN_USER_DATA.username + "new", userData.ADMIN_USER_DATA.password + "new");
+        cy.visit(`/settings/users/edit/${userData.DEFAULT_USER_DATA.id}`);
+        actor.actor.editUserTask.inactivateUser();
+        cy.wait(2000);
 
+    
+    });
+    
+});
+describe("Inactivated user cannot login", () => {
+    it("user cannot login with inactivated account", () => {
+        cy.visit('/');
+        actor.actor.loginTask.fillAndSubmitLoginForm(userData.DEFAULT_USER_DATA.username, userData.DEFAULT_USER_DATA.password);
+        cy.get('.alert')
+            .should("be.visible")
+            .and("contain.text", "Incorrect username or password.");
     });
 });
--- test/cypress/support/pages/settings-users-edit-page.js
+++ test/cypress/support/pages/settings-users-edit-page.js
@@ -1,9 +1,10 @@
-const SettingsUsersEditPage = {
+exports.SettingsUsersEditPage = {
     url: Cypress.env("baseUrl") + "/settings/users/edit",
-    usernameInput: '[data-cy="username-input"]',
-    newPassWordInput: '[data-cy="password-input"]',
-    newPasswordRepeatInput: '[data-cy="password-repeat-input"]',
-    submitEditUserForm: '[data-cy="submit-create-admin-form"]',
-    submitNewPasswordForm: '[data-cy="submit-new-password-form"]',
-    activeStatus: '[data-cy="active-status"]',
+    usernameInput: '[data-cy="edit-username-input"]',
+    currentPassWordInput: '[data-cy="edit-current-password-input"]',
+    newPassWordInput: '[data-cy="edit-new-password-input"]',
+    newPasswordRepeatInput: '[data-cy="edit-new-password-repeat-input"]',
+    submitEditUserNameForm: '[data-cy="submit-edit-username-form"]',
+    submitNewPasswordForm: '[data-cy="submit-edit-password-form"]',
+    activeStatus: '[data-cy="edit-active-checkbox"]',
 };
