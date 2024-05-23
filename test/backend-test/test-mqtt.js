4a2788cf0b9c9a140c0d991d1137dd6dffb22bdf
Frank Elsinga <frank@elsinga.de>
2024-05-24 01:48:59 +0200

    tested a fresh implementation in CI

--- test/backend-test/test-mqtt.js
+++ test/backend-test/test-mqtt.js
@@ -1,12 +1,4 @@
-const semver = require("semver");
-let test;
-const nodeVersion = process.versions.node;
-if (semver.satisfies(nodeVersion, ">= 18")) {
-    test = require("node:test");
-} else {
-    test = require("test");
-}
-
+const test = require("node:test");
 const assert = require("node:assert");
 const { HiveMQContainer } = require("@testcontainers/hivemq");
 const mqtt = require("mqtt");
@@ -14,113 +6,65 @@ const { MqttMonitorType } = require("../../server/monitor-types/mqtt");
 const { UP, PENDING } = require("../../src/util");
 
 /**
- * Runs an MQTT test with the
- * @param  {string} mqttSuccessMessage the message that the monitor expects
- * @param {null|"keyword"|"json-query"} mqttCheckType the type of check we perform
- * @param {string} receivedMessage what message is recieved from the mqtt channel
- * @param {(h:Heartbeat)=>{}} onSuccess  executed if the check succeeds
- * @param {(e:Error)=>{}} onError executed if the check fails
- * @returns {void}
+ * @param connectionString
+ * @param expected
+ * @param mqttCheckType
  */
-function testMqtt(mqttSuccessMessage, mqttCheckType, receivedMessage, onSuccess, onError) {
-    console.info("launching mqtt container");
-    new HiveMQContainer().start().then((hiveMQContainer) => {
-        const connectionString = hiveMQContainer.getConnectionString();
-        const mqttMonitorType = new MqttMonitorType();
-        const monitor = {
-            jsonPath: "firstProp", // always return firstProp for the json-query monitor
-            hostname: connectionString.split(":", 2).join(":"),
-            mqttTopic: "test",
-            port: connectionString.split(":")[2],
-            mqttUsername: null,
-            mqttPassword: null,
-            interval: 20, // controls the timeout
-            mqttSuccessMessage: mqttSuccessMessage, // for keywords
-            expectedValue: mqttSuccessMessage, // for json-query
-            mqttCheckType: mqttCheckType,
-        };
-        const heartbeat = {
-            msg: "",
-            status: PENDING,
-        };
-        mqttMonitorType.check(monitor, heartbeat, {})
-            .then(() => {
-                testMqttClient.end();
-                hiveMQContainer.stop();
-            })
-            .then(() => onSuccess(heartbeat), onError);
+async function runMqttCheck(connectionString, expected, mqttCheckType) {
+    const mqttMonitorType = new MqttMonitorType();
+    const monitor = {
+        jsonPath: "firstProp", // always return firstProp for the json-query monitor
+        hostname: connectionString.split(":", 2).join(":"),
+        mqttTopic: "test",
+        port: connectionString.split(":")[2],
+        mqttUsername: null,
+        mqttPassword: null,
+        interval: 2, // controls the timeout
+        mqttSuccessMessage: expected, // for keywords
+        expectedValue: expected, // for json-query
+        mqttCheckType: mqttCheckType,
+    };
+    const heartbeat = {
+        msg: "",
+        status: PENDING,
+    };
+    await mqttMonitorType.check(monitor, heartbeat, {});
+    return heartbeat;
+}
 
-        const testMqttClient = mqtt.connect(hiveMQContainer.getConnectionString());
+/**
+ * @param conn
+ * @param message
+ */
+async function publishMqtt(conn, message) {
+    await new Promise((resolve, reject) => {
+        const testMqttClient = mqtt.connect(conn);
         testMqttClient.on("connect", () => {
             testMqttClient.subscribe("test", (error) => {
                 if (!error) {
-                    testMqttClient.publish("test", receivedMessage);
+                    testMqttClient.publish("test", message);
+                    testMqttClient.end();
+                    resolve();
+                } else {
+                    reject(error);
                 }
             });
         });
     });
 }
 
-test("MqttMonitorType - valid keywords (type=default)", (t, done) => {
-    testMqtt("KEYWORD", null, "-> KEYWORD <-",
-        (heartbeat) => {
-            assert.strictEqual(heartbeat.status, UP);
-            assert.strictEqual(heartbeat.msg, "Topic: test; Message: -> KEYWORD <-");
-            done();
-        },
-        e => done(e));
-});
-
-test("MqttMonitorType - valid keywords (type=keyword)", (t, done) => {
-    testMqtt("KEYWORD", "keyword", "-> KEYWORD <-",
-        (heartbeat) => {
-            assert.strictEqual(heartbeat.status, UP);
-            assert.strictEqual(heartbeat.msg, "Topic: test; Message: -> KEYWORD <-");
-            done();
-        },
-        e => done(e));
-});
-test("MqttMonitorType - invalid keywords (type=default)", (t, done) => {
-    testMqtt("NOT_PRESENT", null, "-> KEYWORD <-",
-        () => done(new Error("keywords without a keyword should have thrown")),
-        error => {
-            assert.strictEqual(error.message, "Message Mismatch - Topic: test; Message: -> KEYWORD <-");
-            done();
-        });
-});
-test("MqttMonitorType - invalid keyword (type=keyword)", (t, done) => {
-    testMqtt("NOT_PRESENT", "keyword", "-> KEYWORD <-",
-        () => done(new Error("keywords without a keyword should have thrown")),
-        error => {
-            assert.strictEqual(error.message, "Message Mismatch - Topic: test; Message: -> KEYWORD <-");
-            done();
-        });
-});
-test("MqttMonitorType - valid json-query", (t, done) => {
-    // works because the monitors' jsonPath is hard-coded to "firstProp"
-    testMqtt("present", "json-query", "{\"firstProp\":\"present\"}",
-        (heartbeat) => {
-            assert.strictEqual(heartbeat.status, UP);
-            assert.strictEqual(heartbeat.msg, "Message received, expected value is found");
-            done();
-        },
-        e => done(e));
-});
-test("MqttMonitorType - invalid (because query fails) json-query", (t, done) => {
-    // works because the monitors' jsonPath is hard-coded to "firstProp"
-    testMqtt("[not_relevant]", "json-query", "{}",
-        (heartbeat) => done(new Error("json-query an empty message should have thrown, heartbeat=" + JSON.stringify(heartbeat))),
-        error => {
-            assert.strictEqual(error.message, "Message received but value is not equal to expected value, value was: [undefined]");
-            done();
-        });
-});
-test("MqttMonitorType - invalid (because successMessage fails) json-query", (t, done) => {
-    // works because the monitors' jsonPath is hard-coded to "firstProp"
-    testMqtt("[wrong_success_messsage]", "json-query", "{\"firstProp\":\"present\"}",
-        () => done(new Error("json-query an empty message should have thrown")),
-        error => {
-            assert.strictEqual(error.message, "Message received but value is not equal to expected value, value was: [present]");
-            done();
-        });
+test("MqttMonitorType", async (t) => {
+    let container = await (new HiveMQContainer()).start();
+    let conn = container.getConnectionString();
+    console.log("conn", conn);
+    await t.test("timeouts work", async (_t) => {
+        let now = new Date();
+        try {
+            await runMqttCheck(conn, "1", "0");
+            assert.fail("timeouts are thrown");
+        } catch (_e) {}
+        let elapsedTimeSeconds = (new Date()).getTime() - now.getTime();
+        assert.strictEqual(elapsedTimeSeconds > 1000, true);
+    });
+    await container.stop();
 });
